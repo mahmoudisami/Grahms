333a1a8955b870b2c83a29c3e1982e7a05dc158d
Roat Kim <roat95@hotmail.com>
2019-02-01 12:43:39 +0100

    Add Game_screen.java

---
+++ src/ihm/Game_screen.java
@@ -0,0 +1,192 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import javax.swing.JTextPane;
+import javax.swing.JProgressBar;
+
+import clock.Clock; 
+
+public class Game_screen extends JFrame implements Runnable{
+
+	private JPanel contentPane;
+	private static Clock clock;
+	private static JLabel clockLabDate;
+	private static JLabel clockLabTime;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Game_screen frame = new Game_screen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Game_screen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 854, 809);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel gamePanel = new JPanel();
+		gamePanel.setBounds(10, 11, 592, 600);
+		contentPane.add(gamePanel);
+		
+		JPanel infoVillePanel = new JPanel();
+		infoVillePanel.setBounds(612, 11, 216, 289);
+		contentPane.add(infoVillePanel);
+		infoVillePanel.setLayout(null);
+		
+		JTextPane txtpnVille = new JTextPane();
+		txtpnVille.setBounds(93, 0, 32, 20);
+		txtpnVille.setText("VILLE");
+		infoVillePanel.add(txtpnVille);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setBounds(20, 87, 161, 14);
+		infoVillePanel.add(progressBar);
+		
+		JTextPane txtpnPopulationTotale = new JTextPane();
+		txtpnPopulationTotale.setText("Population totale :");
+		txtpnPopulationTotale.setBounds(20, 33, 161, 20);
+		infoVillePanel.add(txtpnPopulationTotale);
+		
+		JButton btnNewButton_3 = new JButton("HISTORIQUE");
+		btnNewButton_3.setBounds(0, 221, 206, 57);
+		infoVillePanel.add(btnNewButton_3);
+		
+		JTextPane txtpnEquilibreFinancier = new JTextPane();
+		txtpnEquilibreFinancier.setText("Equilibre financier :");
+		txtpnEquilibreFinancier.setBounds(20, 125, 161, 20);
+		infoVillePanel.add(txtpnEquilibreFinancier);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setText("+/- :");
+		textPane.setBounds(20, 156, 161, 20);
+		infoVillePanel.add(textPane);
+		
+		JPanel buttonLPanel = new JPanel();
+		buttonLPanel.setBounds(10, 622, 282, 137);
+		contentPane.add(buttonLPanel);
+		buttonLPanel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(82, 11, 115, 31);
+		buttonLPanel.add(btnNewButton);
+		
+		JButton button = new JButton("New button");
+		button.setBounds(82, 53, 115, 31);
+		buttonLPanel.add(button);
+		
+		JButton button_1 = new JButton("New button");
+		button_1.setBounds(82, 95, 115, 31);
+		buttonLPanel.add(button_1);
+		
+		JPanel buttonRPanel = new JPanel();
+		buttonRPanel.setBounds(302, 622, 299, 137);
+		contentPane.add(buttonRPanel);
+		buttonRPanel.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(27, 11, 89, 73);
+		buttonRPanel.add(btnNewButton_1);
+		
+		JButton button_2 = new JButton("New button");
+		button_2.setBounds(166, 11, 89, 73);
+		buttonRPanel.add(button_2);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(27, 95, 228, 23);
+		buttonRPanel.add(btnNewButton_2);
+		
+		JPanel infoDistrictPanel = new JPanel();
+		infoDistrictPanel.setBounds(612, 311, 216, 223);
+		contentPane.add(infoDistrictPanel);
+		infoDistrictPanel.setLayout(null);
+		
+		JTextPane txtpnQuartier = new JTextPane();
+		txtpnQuartier.setBounds(79, 5, 58, 20);
+		txtpnQuartier.setText("QUARTIER");
+		infoDistrictPanel.add(txtpnQuartier);
+		
+		JTextPane txtpnPopulation = new JTextPane();
+		txtpnPopulation.setText("Population :");
+		txtpnPopulation.setBounds(23, 52, 153, 20);
+		infoDistrictPanel.add(txtpnPopulation);
+		
+		JTextPane txtpnNombreDeVisitejours = new JTextPane();
+		txtpnNombreDeVisitejours.setText("Nombre de visite / jours :");
+		txtpnNombreDeVisitejours.setBounds(23, 120, 153, 20);
+		infoDistrictPanel.add(txtpnNombreDeVisitejours);
+		
+		JPanel infoDatePanel = new JPanel();
+		infoDatePanel.setBounds(612, 545, 216, 214);
+		contentPane.add(infoDatePanel);
+		infoDatePanel.setLayout(null);
+		
+		JTextPane txtpnDate = new JTextPane();
+		txtpnDate.setBounds(92, 5, 32, 20);
+		txtpnDate.setText("DATE");
+		infoDatePanel.add(txtpnDate);
+		
+		JButton button_3 = new JButton(">>>");
+		button_3.setBounds(85, 41, 64, 23);
+		infoDatePanel.add(button_3);
+		
+		JTextPane txtpnX = new JTextPane();
+		txtpnX.setText("x 10");
+		txtpnX.setBounds(159, 44, 39, 20);
+		infoDatePanel.add(txtpnX);
+		
+		JTextPane txtpnVitesse = new JTextPane();
+		txtpnVitesse.setBounds(28, 44, 47, 20);
+		infoDatePanel.add(txtpnVitesse);
+		txtpnVitesse.setText("Vitesse :");
+		
+		clock = new Clock();
+		clockLabDate = new JLabel(clock.displayDate());
+		clockLabDate.setBounds(60, 97, 98, 37);
+		infoDatePanel.add(clockLabDate);
+		
+		clockLabTime = new JLabel(clock.displayTime());
+		clockLabTime.setBounds(70, 133, 112, 30);
+		infoDatePanel.add(clockLabTime);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while (true) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+			clock.increment();
+			clockLabDate.setText(clock.displayDate());
+			clockLabTime.setText(clock.displayTime());
+		}
+	}
+}
