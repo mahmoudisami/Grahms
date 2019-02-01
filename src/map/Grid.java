f863c3d153dd3289cc62481bf22af72436ff4104
Antoine Leguay <antoinelg@hotmail.fr>
2019-02-01 16:43:41 +0100

    Début de la classe grid.

--- src/map/Grid.java
+++ src/map/Grid.java
@@ -1,5 +1,27 @@
 package map;
 
-public class Grid {
-	int a;
+import javax.swing.JPanel;
+
+import data.District;
+
+public class Grid extends JPanel{
+	
+	int Taille = 5;
+	District[][] grid = new District[Taille][Taille];
+	
+	public Grid(){
+		for(int lig=0;lig<Taille;lig++){
+    		for(int col=0;col<Taille;col++){
+    			grid[lig][col] = null;
+    		}
+    	}
+	}
+	
+	public void updateGrid(int x,int y,District dist){
+		
+		//conversion pixel vers case du tab
+		
+		
+	}
+
 }
