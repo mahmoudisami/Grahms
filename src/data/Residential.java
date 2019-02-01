861ca9db0bc1b8512ac4d5a6d4ceb18cb0861f00
mvoisin78 <mvoisin@hotmail.fr>
2019-02-01 16:48:53 +0100

    Img District

--- src/data/Residential.java
+++ src/data/Residential.java
@@ -4,9 +4,28 @@ public class Residential extends District{
 
 	private int size;
 	private int maxPeople;
+	private String img1 = "/src/img/residLv1.png";
+	private String img2 = "/src/img/residLv2.png";
+	private String img3 = "/src/img/residLv3.png";
 	
 	public Residential() {
 		super(Const.GAIN_RES,0); 
 		size = 1; // Taille de dé part
+	}
+	
+	public String getImg() {
+		if(size == 1) {
+			return img1;
+		}
+		else if(size == 2) {
+			return img2;
+		}
+		else {
+			return img3;
+		}
+	}
+	
+	public void upSize() {
+		size++;
 	}
 }
--- src/data/Services.java
+++ src/data/Services.java
@@ -3,10 +3,28 @@ package data;
 public class Services extends District{
 
 	private int size;
-
+	private String img1 = "/src/img/ServLv1.png";
+	private String img2 = "/src/img/ServLv2.png";
+	private String img3 = "/src/img/ServLv3.png";
 	 
 	public Services() {
 		super(Const.GAIN_SERV,Const.COST_SERV);
 		size = 1; // Taille de départ
+	}
+	
+	public String getImg() {
+		if(size == 1) {
+			return img1;
+		}
+		else if(size == 2) {
+			return img2;
+		}
+		else {
+			return img3;
+		}
+	}
+	
+	public void upSize() {
+		size++;
 	}
 }
