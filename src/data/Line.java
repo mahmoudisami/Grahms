d0d8ad91f7ed20a6c1cf2ebb89068a69fd5c9cd0
mvoisin78 <mvoisin@hotmail.fr>
2019-02-01 15:44:29 +0100

    Line class added

---
+++ src/data/Line.java
@@ -0,0 +1,14 @@
+package data;
+
+public class Line {
+
+	private Station firstStation;
+	private Station secondStation;
+	private int visitedDistrict;
+	
+	public Line(Station firstStation, Station secondStation) {
+		this.firstStation = firstStation;
+		this.secondStation = secondStation;
+	}
+	
+}
