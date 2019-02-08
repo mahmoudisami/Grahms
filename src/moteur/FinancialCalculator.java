8896608efef4ea060a68307b4b4a57c2af48a954
mvoisin78 <mvoisin@hotmail.fr>
2019-02-08 16:34:21 +0100

    .

--- src/moteur/FinancialCalculator.java
+++ src/moteur/FinancialCalculator.java
@@ -13,24 +13,24 @@ public class FinancialCalculator {
 		totalDistrictGain = 0;
 	}
 	
-	public int districtCalculatorGain(District tab[][], int dim) {
+	public int districtCalculatorGain(District grid[][], int dim) {
 		totalDistrictGain = 0;
 		for(int i=0; i<dim; i++) {
 			for(int j=0; j<dim; j++) {
-				if(tab[i][j] != null) {
-					totalDistrictGain += tab[i][j].getGain();
+				if(grid[i][j] != null) {
+					totalDistrictGain += grid[i][j].getGain();
 				}
 			}
 		}
 		return totalDistrictGain;
 	}
 	
-	public int districtCalculatorCost(District tab[][], int dim) {
+	public int districtCalculatorCost(District grid[][], int dim) {
 		totalDistrictCost = 0;
 		for(int i=0; i<dim; i++) {
 			for(int j=0; j<dim; j++) {
-				if(tab[i][j] != null) {
-					totalDistrictCost += tab[i][j].getCost();
+				if(grid[i][j] != null) {
+					totalDistrictCost += grid[i][j].getCost();
 				}
 			}
 		}
