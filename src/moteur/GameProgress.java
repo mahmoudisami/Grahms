98c7e9e7081689c7825c261be44ded6eaf957d48
Gabriel <Gabriel@PC-Gab>
2019-02-15 17:00:42 +0100

    La fonction LauchGameProgress va prendre en paramètre la clock et executer les fonctions selon le jour ou leur ect

--- src/moteur/GameProgress.java
+++ src/moteur/GameProgress.java
@@ -1,26 +1,20 @@
 package moteur;
 
+import data.*;
+import ihm.*;
+import moteur.*;
 
 public class GameProgress {
 	
 	// private long speed = 1000; à mettre dans gameScreen
 	private static Clock clock;
 	
-	public void GameProgress(long speed){
-		String dayName = clock.getDayName();
-		int dayCount = clock.getDayCpt();
-		String hour = clock.getHour();
-		
-		while (true) {
-			try {
-				Thread.sleep(speed);
-			} catch (InterruptedException e) {
-				System.out.println(e.getMessage());
-			}
-			
-			
-			clock.increment();		
-			// a set (clock.displayGameTimeInfo()); puis get dans ihm
+	public void LaunchGameProgress (Clock clock, Grid grid) {
+		if(clock.getDayCpt()==5) { //exemple
+
+		}
+		if(clock.getHour()=="7:00") {
+			//APPEL fonction habitants parte au travail
 		}
 	}
 }
