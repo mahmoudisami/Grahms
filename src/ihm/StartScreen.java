bb71c365966496c1e8b5675dae78481c1d845d52
Roat Kim <roat95@hotmail.com>
2019-02-22 14:57:18 +0100

    Added Rules.txt and Rules windows on StartScreen

--- src/ihm/StartScreen.java
+++ src/ihm/StartScreen.java
@@ -5,9 +5,15 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class StartScreen extends JFrame {
@@ -66,9 +72,42 @@ public class StartScreen extends JFrame {
 		button.setBounds(218, 202, 223, 62);
 		panel.add(button);
 		
-		JButton button_1 = new JButton("Help");
-		button_1.setBounds(218, 285, 223, 62);
-		panel.add(button_1);
+		JButton btnRules = new JButton("Rules");
+		btnRules.setBounds(218, 285, 223, 62);
+		btnRules.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame ruleScreen = new JFrame();
+				ruleScreen.setSize(500,510);
+				ruleScreen.setTitle("Rules");
+				ruleScreen.setLocationRelativeTo(null);
+				//JTextArea ruleText = new JTextArea("",5,10);
+				JTextArea ruleText = new JTextArea(10,10);
+               
+                try {
+                	FileReader reader = null;
+                	reader = new FileReader( "src/document/Rules.txt" );
+                	BufferedReader br = new BufferedReader(reader);
+					ruleText.read(br, null);
+					br.close();
+                    ruleText.requestFocus();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                
+				ruleScreen.getContentPane().add(new JScrollPane(ruleText));
+				ruleScreen.setVisible(true);
+				ruleText.setEditable(false);
+				btnRules.setEnabled(false);
+				ruleScreen.addWindowListener(new java.awt.event.WindowAdapter() {
+					public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+						btnRules.setEnabled(true);
+					}
+				});
+			}	
+		});
+		panel.add(btnRules);
 		
 		JButton button_2 = new JButton("Quit");
 		button_2.addActionListener(new ActionListener() {
