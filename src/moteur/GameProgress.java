003cb01d5eaf5424beaf509f04a34898f21163cf
mvoisin78 <mvoisin@hotmail.fr>
2019-02-22 14:15:43 +0100

    bon calcul mdr

--- src/moteur/GameProgress.java
+++ src/moteur/GameProgress.java
@@ -6,14 +6,17 @@ import moteur.*;
 
 public class GameProgress {
 	
-	// private long speed = 1000; à mettre dans gameScreen
+	// private long speed = 1000; ï¿½ mettre dans gameScreen
 	private static Clock clock;
+	private static FinancialCalculator fin = new FinancialCalculator();
+	private static int tmpMoney;
 	
-	public void LaunchGameProgress (Clock clock, Grid grid) {
-		if(clock.getDayCpt()==5) { //exemple
-
+	public void launchGameProgress (Clock clock, Grid grid) {
+		if(clock.getDayPos()==7 && clock.getHour().equals("23")) { //exemple
+			tmpMoney = fin.districtCalculatorCost(grid.getMapTab(), grid.getMapTab().length);
+			System.out.println(tmpMoney);
 		}
-		if(clock.getHour()=="7:00") {
+		if(clock.getHour().equals("07")) {
 			//APPEL fonction habitants parte au travail
 		}
 	}
