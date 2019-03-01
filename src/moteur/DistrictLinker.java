e76e66e6949983e0f1b8b996c42b171db625f993
mvoisin78 <mvoisin@hotmail.fr>
2019-03-01 15:59:26 +0100

    Modifications

--- src/moteur/DistrictLinker.java
+++ src/moteur/DistrictLinker.java
@@ -18,12 +18,16 @@ public class DistrictLinker {
 		int distance = line.getDistance();
 		District d1 = line.getFirstDistrict();
 		District d2 = line.getSecondDistrict();
+		System.out.println("----"+ d1 +"  " + d2+"----");
 		addAD(d1,d2,distance);
 		addAD(d2,d1,distance);
+		refreshAD(d1,d2, distance);
+		refreshAD(d2,d1, distance);
 	}
 	
 	public void addAD(District dist1, District dist2,int distance) {
 		dist1.addAccessibleDistrict(dist2, distance);
+		//System.out.println("Ajout de "+ dist2 +" dans " + dist1);
 		ArrayList<AccessibleDistrict> aDistrict1 = dist1.getAccessibleDistrict();
 		ArrayList<AccessibleDistrict> aDistrict2 = dist2.getAccessibleDistrict();
 		Boolean isAccessible;
@@ -33,15 +37,52 @@ public class DistrictLinker {
 		for(int index = 0; index < size;index++) {
 			isAccessible  = false;
 			for(int j=0; j< aDistrict2.size(); j++) {
-				if(aDistrict1.get(index) == aDistrict2.get(j)) {
+				if(aDistrict1.get(index).getDistrict() == aDistrict2.get(j).getDistrict()) {
 					isAccessible = true;
 				}
 			}
 			if(!isAccessible && aDistrict1.get(index).getDistrict()!= dist2) {
 				newDistance = distance + aDistrict1.get(index).getDistance();
 				newDistrict = aDistrict1.get(index).getDistrict();
+				//System.out.println("..Ajout de: "+ newDistrict + " dans " + dist2 + " avec dist1 = "+dist1 +" et distance " + newDistance);
 				dist2.addAccessibleDistrict(newDistrict, newDistance);
 			}
 		}
+	}
+	
+	public void refreshAD(District dist1, District dist2, int distance) {
+		ArrayList<AccessibleDistrict> aDistrict1 = dist1.getAccessibleDistrict();
+		ArrayList<AccessibleDistrict> newAccessible;
+		District addDistrict;
+		int newDistance;
+		int size = aDistrict1.size();
+		int sizeNA;
+		for(int index = 0; index<size; index++) {
+			System.out.println("<--->");
+			//isAccesible = false;
+			addDistrict = aDistrict1.get(index).getDistrict();
+			newAccessible = addDistrict.getAccessibleDistrict();
+			sizeNA = newAccessible.size();
+			for(int j=0; j<sizeNA;j++) {
+				if((newAccessible.get(j).getDistrict() != dist2) && (newAccessible.get(j).getDistrict() != dist1) && (addDistrict != dist2)) {
+					System.out.println("newAccessible "+ newAccessible.get(j).getDistrict() + " et dist 1 = " + dist1 + " et dist2 = "+dist2 );
+					if(!containsDistrict(newAccessible, dist2)) {
+						System.out.println("add");
+						newDistance = distance + 10;
+						addDistrict.addAccessibleDistrict(dist2, newDistance);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	public boolean containsDistrict(ArrayList<AccessibleDistrict> aDistrict, District d) {
+		for(int i=0; i<aDistrict.size();i++) {
+			if(aDistrict.get(i).getDistrict() == d) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/moteur/TestCalculator.java
+++ src/moteur/TestCalculator.java
@@ -8,7 +8,7 @@ public class TestCalculator {
 	public static void main(String[] args) {
 		int size = 5;
 		District[][] grid = new District[size][size];
-		FinancialCalculator calc = new FinancialCalculator();
+		//FinancialCalculator calc = new FinancialCalculator();
 		DistrictLinker distLink = new DistrictLinker();
 		
 		for(int i=0;i<size;i++) {
@@ -17,26 +17,23 @@ public class TestCalculator {
 			}
 		}
 		
-		grid[1][0] = new Residential();
 		grid[0][0] = new Services();
-		grid[1][1] = new Residential();
+		grid[1][0] = new Residential();
+		grid[1][1] = new District(1,1);
 		grid[2][2] = new Commercial();
 		
 		Line line1 = new Line(grid[0][0],grid[1][0],1);
 		Line line2 = new Line(grid[0][0],grid[2][2],3);
 		Line line3 = new Line(grid[1][1],grid[2][2],2);
 		
-		grid[1][0].createStation();
-		grid[0][0].createStation();
-		grid[1][1].createStation();
-		grid[2][2].createStation();
-		
 		distLink.linkDistrict(line1);
 		distLink.linkDistrict(line2);
 		distLink.linkDistrict(line3);
 		
-		for(int i = 0; i<grid[1][1].getAccessibleDistrict().size();i++) {
-			System.out.println(grid[1][1].getAccessibleDistrict().get(i).getDistrict().toString() + "  " + grid[1][1].getAccessibleDistrict().get(i).getDistance());	
+		System.out.println("------------------------------------------");
+		District dist = grid[0][0];
+		for(int i = 0; i<dist.getAccessibleDistrict().size();i++) {
+			System.out.println(dist.getAccessibleDistrict().get(i).getDistrict().toString() + "  " + dist.getAccessibleDistrict().get(i).getDistance());	
 		}
 		
 	}
